ad8ee4a5b3ac7a848fa74dc915688c8a986ea10b
baffeto <baffeto@mail.ru>
2023-06-14 19:14:45 +0300

    gfdgfd

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,19 +4,20 @@ import ProductItem from "../ProductItem/ProductItem";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 500, description: "Синий цвет, прямые"},
-    {id: '2', title: 'Джинсы 2', price: 300, description: "Красный цвет, прямые"},
-    {id: '3', title: 'Джинсы 3', price: 600, description: "Синий цвет, непрямые"},
-    {id: '4', title: 'Джинсы 4', price: 1200, description: "Желтый цвет, прямые"},
-    {id: '5', title: 'Джинсы 5', price: 6500, description: "Голубой цвет, прямые"},
-    {id: '6', title: 'Джинсы 6', price: 2500, description: "Черный цвет, непрямые"},
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
-    });
-
+    }, 0)
 }
 
 

83036105cae31765d670ae2ca42f88f7632e5ce9
baffeto <baffeto@mail.ru>
2023-06-14 15:04:59 +0300

    gdfgfd

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './Form.css'
 import { useTelegram } from "../../hooks/useTelegram";
+import { useEffect } from 'react';
 
 const Form = () => {
     const [country, setCountry] = useState('');
